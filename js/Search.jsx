b76b97bda1f63f71e6a8025b28e9ead0cee23a2f
snackersteph <swongwrites@gmail.com>
2018-05-24 15:44:46 -0700

    add onChange function to Search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className='search'>
-    <div>
-      {preload.shows.map( (show) => <ShowCard show={show} key={show.imdbID} />  )}
+class Search extends Component {
+  state = {
+    searchTerm: 'this is some sort of debug statement'
+  }
+
+  handleSearchTermChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  render() {
+    return(
+    <div className='search'>
+      <header>
+        <h1>svideo</h1>
+        <input 
+          onChange={this.handleSearchTermChange} 
+          value={this.state.searchTerm} 
+          type='text' 
+          placeholder='Search' 
+        />
+      </header>
+      <div>
+        {preload.shows.map( (show) => <ShowCard show={show} key={show.imdbID} />  )}
+      </div>
     </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Search;
