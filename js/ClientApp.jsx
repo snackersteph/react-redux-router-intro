aef8647d9cd33555ceae91a19320f9b91263ff77
snackersteph <swongwrites@gmail.com>
2018-05-24 14:23:12 -0700

    Implement HashRouter

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const ce = React.createElement;
-
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'Lime' })
+const App = () => {
+  return (
+    <HashRouter>
+      <div className='app'>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+      </div>
+    </HashRouter>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
