e60219209b799c33857a729f23e5dcbabae06a7d
snackersteph <swongwrites@gmail.com>
2018-05-24 15:17:51 -0700

    Integrate styled-components

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
+import styled from 'styled-components';
+
+// this is a tagged template literal which allows you to parse template literals with a function
+// styled-components library interprets this
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 1-px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props => (
-  <div className='show-card'>
-    <img alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
+  <Wrapper>
+    <Image alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
     <div>
       <h3>{props.show.title}</h3>
       <h4>({props.show.year})</h4>
       <p>{props.show.description}</p>
     </div>
-  </div>
+  </Wrapper>
 );
+
+ShowCard.propTypes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+}
 
 export default ShowCard;
