6e701bce5d82e62a4905f223debc35941574c34a
snackersteph <swongwrites@gmail.com>
2018-05-29 17:16:47 -0700

    Implement server-side rendering

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,19 +2,19 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-// import Perf from 'react-addons-perf';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// Only used for development
-// window.Perf = Perf;
-// Perf.start();
-
 const renderApp = () => {
-  render(<App />, document.getElementById('app'));
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('app')
+  );
 };
 renderApp();
 
-// the module variable is coming from webpack
 if (module.hot) {
   module.hot.accept('./App', () => {
     renderApp();
